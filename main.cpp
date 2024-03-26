36d01678d0e2807302b93e31ae66baf564c32dc1
Jora <jora.4.gevorgyan@gmail.com>
2024-03-26 07:45:13 +0400

    refactoring

--- main.cpp
+++ main.cpp
@@ -36,10 +36,11 @@ std::vector<std::string> get_args(argparse::ArgumentParser &parser,
 void run_brctl_nl(argparse::ArgumentParser &parser) {
   BrctlNetlink brctl_nl{};
   brctl_nl.init();
-  if (parser.exists(kw::SHOW)) {
-    brctl_nl.show();
-  }
   do {
+    if (parser.exists(kw::SHOW)) {
+      brctl_nl.show();
+      break;
+    }
     if (parser.exists(kw::ADD)) {
       auto args = get_args(parser, kw::ADD, kw::ONE);
       if (!args.empty()) {
@@ -49,28 +50,27 @@ void run_brctl_nl(argparse::ArgumentParser &parser) {
     }
     if (parser.exists(kw::ADDIF)) {
       auto args = get_args(parser, kw::ADDIF, kw::TWO);
-      if (args.empty()) {
+      if (!args.empty()) {
         brctl_nl.addif(args[0], args[1]);
       }
       break;
     }
     if (parser.exists(kw::DEL)) {
       auto args = get_args(parser, kw::DEL, kw::ONE);
-      if (args.empty()) {
+      if (!args.empty()) {
         brctl_nl.del(args[0]);
       }
       break;
     }
     if (parser.exists(kw::DELIF)) {
       auto args = get_args(parser, kw::DELIF, kw::TWO);
-      if (args.empty()) {
+      if (!args.empty()) {
         brctl_nl.delif(args[0], args[1]);
       }
       break;
     }
+    parser.print_help();
   } while (false);
-
-  parser.print_help();
 }
 
 int main(int argc, const char *argv[]) {
